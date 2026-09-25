5ddaac46770b90036bbce74f269749bc467a34ce
jrollaislarno <jrollaislarno@350028R-F115X06.lise.kreizenn.bzh>
2026-09-25 15:04:56 +0200

    Permutation de variables

--- src/pkg00/helloworld/HelloWorld.java
+++ src/pkg00/helloworld/HelloWorld.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package pkg00.helloworld;
+import java.util.Scanner;
 
 /**
  *
@@ -14,7 +15,18 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc =new Scanner(System.in);
+        
+        System.out.print("x : ");
+        int x = sc.nextInt();
+        
+        System.out.print("y : ");
+        int y = sc.nextInt();
+        
+        System.out.println("x " +x +" y : " +y);
+        
+        //System.out.println("Hello");
+        //System.out.println("World!");
     }
     
 }
